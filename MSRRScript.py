d534106563525fab323105373424f1006f5ea200
Cheng Jun-Yan <chengjunyan1@gmail.com>
2019-12-08 16:21:39 -0500

    Update MSRRScript.py

--- MSRRScript.py
+++ MSRRScript.py
@@ -9,11 +9,3 @@ X=MSRR(init)
 
 X.reconfig(C3)
 X.reconfig(C4)
-
-# save_path=os.path.join(root_path,'temp')
-# img_path=os.path.join(save_path,'frame.jpg')
-# img=cv2.imread(img_path)
-# frame_rs=cv2.resize(img, (int(img.shape[1]/2),int(img.shape[0]/2)), interpolation=cv2.INTER_CUBIC)
-# cv2.imshow('Path',frame_rs)
-# cv2.waitKey(10000)
-# print(1111)
